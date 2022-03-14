e3ef971060af8b42b1ed427da7ee7b629ed50c55
PedroJColon <PJColon@student.fullsail.edu>
2022-03-13 20:07:21 -0400

    Added quit game functionailty

--- Assets/Scripts/StartMenuScript.cs
+++ Assets/Scripts/StartMenuScript.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StartMenuScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{    
+    public void QuitGame()
     {
-        
+        Debug.Log("QUIT");
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
